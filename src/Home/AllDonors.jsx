72b247733b6e5377fbf05fd63bde68e75b4881e2
Rifat-Earth <rifathasan01915@gmail.com>
2025-07-24 13:12:21 +0600

    vercel deploy link added

--- src/Home/AllDonors.jsx
+++ src/Home/AllDonors.jsx
@@ -4,7 +4,7 @@ const AllDonors = () => {
     const [donations, setDonations] = useState([]);
 
     useEffect(() => {
-        fetch("http://localhost:5000/donations")
+        fetch("https://blood-collection-server.vercel.app/donations")
             .then((res) => res.json())
             .then((data) => setDonations(data))
             .catch((error) => console.error("Error fetching donations:", error));
